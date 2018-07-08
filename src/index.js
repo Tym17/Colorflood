7e3319f7fa3ffb5d0aa7e6e641ae83abce3a3693
Tym17 <Tym17@users.noreply.github.com>
2018-07-08 12:17:23 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import './fonts.css';
+import Game from './Game';
+
+ReactDOM.render(
+<Game width={16} height={16}/>, 
+document.getElementById('root'));
+
